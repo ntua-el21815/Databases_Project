3c501fb2f3525883a329cf9452c4d2737740b626
NicholasAgg <el21815@mail.ntua.gr>
2024-05-15 16:18:47 +0300

    Created One Episode Genration Scirpt.
    Multiple episode generation script not needed.
    Now all episodes are in the DML.

--- Episode_Gen.py
+++ Episode_Gen.py
@@ -2,6 +2,9 @@ import os
 import mysql.connector
 import random
 
+# Commented Lines were for writing to the DML_SCRIPT.sql file
+# Please don't uncomment them unless you want to write to the file.
+
 #$Env:<variable-name> = "<new-value>" - to set environment variable in PowerShell
 #$Env:MYSQL_KEY = 'password123' - example
 
@@ -13,9 +16,11 @@ mydb = mysql.connector.connect(
 )
 
 if __name__ == "__main__":
+    #DML = open("DML_SCRIPT.sql", "a")
+    #DML.write("\n")
     mycursor = mydb.cursor()
-    STARTING_YEAR = 2018
-    ENDING_YEAR = 2024
+    STARTING_YEAR = int(input("Enter the starting year: "))
+    ENDING_YEAR = int(input("Enter the ending year: "))
     SEASON_EPISODES = 10
     last_3_rec = {0: [] , 1: [], 2: []}
     last_3_chefs = {0: [] , 1: [], 2: []}
@@ -26,6 +31,7 @@ if __name__ == "__main__":
         persistent_chefs = []
         for episode in range(1, SEASON_EPISODES + 1):
             mycursor.execute("INSERT INTO Episodes (episode_number,year_played) VALUES (%s, %s)", (episode,year))
+            #DML.write(f"INSERT INTO Episodes (episode_number,year_played) VALUES ({episode},{year});\n")
             episode_id = mycursor.lastrowid
             mydb.commit()
             print(f"Inserted {year} episode {episode}")
@@ -72,6 +78,7 @@ if __name__ == "__main__":
                 cuisines_used.append(cuisine)
                 recipes_used.append(random_recipe)
                 mycursor.execute("INSERT INTO Chefs_Recipes_Episode (chef_id, recipe_id, episode_id) VALUES (%s, %s, %s)", (chef[0], random_recipe[0], episode_id))
+                #DML.write(f"INSERT INTO Chefs_Recipes_Episode (chef_id, recipe_id, episode_id) VALUES ({chef[0]}, {random_recipe[0]}, {episode_id});\n")
                 chosen_chefs += 1
                 contestants.append(chef)
                 mydb.commit()
@@ -91,6 +98,7 @@ if __name__ == "__main__":
                 judges = random.sample(candidate_judges, 3)
             for judge in judges:
                 mycursor.execute("INSERT INTO is_judge (judge_id, episode_id) VALUES (%s, %s)", (judge[0], episode_id))
+                #DML.write(f"INSERT INTO is_judge (judge_id, episode_id) VALUES ({judge[0]}, {episode_id});\n")
                 mydb.commit()
                 print(f"Inserted judge {judge[0]} for episode {episode_id}")
             last_3_judges[0] = last_3_judges[1]
@@ -103,6 +111,11 @@ if __name__ == "__main__":
                 mycursor.execute("INSERT INTO rates (judge_id, contestant_id, episode_id, score) VALUES (%s, %s, %s, %s)", (judges[0][0], contestant[0], episode_id, score_1))
                 mycursor.execute("INSERT INTO rates (judge_id, contestant_id, episode_id, score) VALUES (%s, %s, %s, %s)", (judges[1][0], contestant[0], episode_id, score_2))
                 mycursor.execute("INSERT INTO rates (judge_id, contestant_id, episode_id, score) VALUES (%s, %s, %s, %s)", (judges[2][0], contestant[0], episode_id, score_3))
+                #DML.write(f"INSERT INTO rates (judge_id, contestant_id, episode_id, score) VALUES ({judges[0][0]}, {contestant[0]}, {episode_id}, {score_1});\n")
+                #DML.write(f"INSERT INTO rates (judge_id, contestant_id, episode_id, score) VALUES ({judges[1][0]}, {contestant[0]}, {episode_id}, {score_2});\n")
+                #DML.write(f"INSERT INTO rates (judge_id, contestant_id, episode_id, score) VALUES ({judges[2][0]}, {contestant[0]}, {episode_id}, {score_3});\n")
                 mydb.commit()
                 print(f"Inserted scores for contestant {contestant[0]} in episode {episode_id} by judges {judges[0][0]}, {judges[1][0]}, {judges[2][0]}")
+            #DML.write("\n")
+    #DML.close()
     mycursor.close()
