6589adb603765bc4211cbdd9d349f2f83c1f60ac
NicholasAgg <el21815@mail.ntua.gr>
2024-05-15 16:19:50 +0300

    Attempt at making admin user.

--- Chef_User.sql
+++ Chef_User.sql
@@ -1,6 +1,6 @@
 /*This creates a chef user with the privileges to SELECT, INSERT, UPDATE and ALTER ON the recipes and chefs tables*/
 
-CREATE USER 'chef'@'localhost' IDENTIFIED BY 'password';/*Change password to your desired password*/
+CREATE USER 'chef'@'localhost' IDENTIFIED BY 'chef123';/*Change password to your desired password*/
 GRANT SELECT, INSERT, UPDATE, ALTER ON cookingcontest.recipes TO 'chef'@'localhost';
 GRANT SELECT, INSERT, UPDATE, ALTER ON cookingcontest.chefs TO 'chef'@'localhost';
 FLUSH PRIVILEGES;  -- This ensures that the privileges are refreshed and applied.
@@ -8,3 +8,8 @@ FLUSH PRIVILEGES;  -- This ensures that the privileges are refreshed and applied
 
 /*After creating the user, click on the Database tab,select Manage Connections, and then click on the Add Connection button.
 Use the credentials of the user you just created.Finally, click on the connect to Database option,also under the Database tab*/
+
+CREATE USER 'admin'@'localhost' IDENTIFIED BY "admin123";/*Change password to your desired password*/
+
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;  -- This ensures that the privileges are refreshed and applied.
