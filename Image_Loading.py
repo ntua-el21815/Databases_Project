d9443d038274c73bd601db6ac1a09a42c51482a3
NicholasAgg <el21815@mail.ntua.gr>
2024-05-16 21:46:54 +0300

    Added Themes and FoodGroups Images.

--- Image_Loading.py
+++ Image_Loading.py
@@ -29,25 +29,40 @@ with open("Recipes.json", "r", encoding="utf-8") as f1:
             print(needed_image)
             image_path = os.path.join(search_path + "\\" + needed_image).replace("\\","\\\\")
             cursor.execute(f"INSERT INTO Images (id,image,descr) VALUES ({i},LOAD_FILE('{image_path}') , '{recipes_info[i-1]['name']}');")
-        mydb.commit()
         for i in range(1, len(recipes_info) + 1):
             cursor.execute(f"UPDATE Recipes SET image_id = {i} WHERE id = {i};")
-        mydb.commit()
         image_path = os.path.join(search_path + "\\chefAvatar.png").replace("\\","\\\\")
         cursor.execute(f"INSERT INTO Images (image,descr) VALUES (LOAD_FILE('{image_path}') , 'Chef Avatar');")
-        mydb.commit()
         image_id = len(recipes_info) + 1
         cursor.execute(f"UPDATE Chefs SET image_id = {image_id};")
         image_path = os.path.join(search_path + "\\ingredientPlaceholder.png").replace("\\","\\\\")
         cursor.execute(f"INSERT INTO Images (image,descr) VALUES (LOAD_FILE('{image_path}') , 'Ingredient Placeholder');")
-        mydb.commit()
         image_id += 1
         cursor.execute(f"UPDATE Ingredients SET image_id = {image_id};")
         image_path = os.path.join(search_path + "\\EpisodePlaceholder.jpg").replace("\\","\\\\")
         cursor.execute(f"INSERT INTO Images (image,descr) VALUES (LOAD_FILE('{image_path}') , 'Episode Placeholder');")
-        mydb.commit()
         image_id += 1
         cursor.execute(f"UPDATE Episodes SET image_id = {image_id};")
+        for i in range(1,38):
+            for file in files:
+                if file.startswith("Theme_ (" + str(i) + ")"):
+                    print(file)
+                    image_path = os.path.join(search_path + "\\" + file).replace("\\","\\\\")
+                    cursor.execute(f"SELECT name FROM Themes WHERE id = {i};")
+                    theme_name = cursor.fetchone()[0]
+                    cursor.execute(f"INSERT INTO Images (image,descr) VALUES (LOAD_FILE('{image_path}') , '{theme_name}');")
+                    image_id = cursor.lastrowid
+                    cursor.execute(f"UPDATE Themes SET image_id = {image_id} WHERE id = {i};")
+        for i in range(1, 12):
+            for file in files:
+                if file.startswith("Food_group (" + str(i) + ")"):
+                    print(file)
+                    image_path = os.path.join(search_path + "\\" + file).replace("\\","\\\\")
+                    cursor.execute(f"SELECT name FROM FoodGroups WHERE id = {i};")
+                    group_name = cursor.fetchone()[0]
+                    cursor.execute(f"INSERT INTO Images (image,descr) VALUES (LOAD_FILE('{image_path}') , '{group_name}');")
+                    image_id = cursor.lastrowid
+                    cursor.execute(f"UPDATE FoodGroups SET image_id = {image_id} WHERE id = {i};")
         mydb.commit()
         cursor.close()
     except Exception as e:
